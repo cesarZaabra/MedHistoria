f4829c5aaf3e3a22ff16ed56858b79a44809754b
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-04 16:38:04 -0500

    404

---
+++ wp-content/themes/med-historia/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Contacto
+ */
+get_header();
+?>
+    <!-- Error Page -->
+    <section class="error-page section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3 col-12">
+                    <!-- Error Inner -->
+                    <div class="error-inner">
+                        <h1>404<span>¡Lo siento, no podemos encontrar esa página!</span></h1>
+                        <p>Puede regresar a <a href="medhistoria.com">MedHistoria.com</a> o ingresar su búsqueda aquí</p>
+                        <form class="search-form">
+                            <input placeholder="Search from Here" type="text">
+                            <button class="btn" type="submit"><i class="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                    <!--/ End Error Inner -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Error Page -->
+<?php get_footer();
