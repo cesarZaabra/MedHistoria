e92cd01691b7383ea84b9d2292db1f790dd8f7eb
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-05 16:05:46 -0500

    footer

--- wp-content/themes/med-historia/footer.php
+++ wp-content/themes/med-historia/footer.php
@@ -6,62 +6,47 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-12">
                     <div class="single-footer">
-                        <h2>About Us</h2>
-                        <p>Lorem ipsum dolor sit am consectetur adipisicing elit do eiusmod tempor incididunt ut labore dolore magna.</p>
+                        <img src="<?= get_template_directory_uri()?>/assets/img/logo-Medhistoria-blanco.png" class="img-fluid">
+                        <br>
+                        <br>
+                        <p>MedHistoria es un software médico tecnológico al servicio de la salud, donde podrá gestionar todas sus historias clínicas de diferentes especialidades.</p>
                         <!-- Social -->
                         <ul class="social">
                             <li><a href="#"><i class="icofont-facebook"></i></a></li>
-                            <li><a href="#"><i class="icofont-google-plus"></i></a></li>
-                            <li><a href="#"><i class="icofont-twitter"></i></a></li>
-                            <li><a href="#"><i class="icofont-vimeo"></i></a></li>
-                            <li><a href="#"><i class="icofont-pinterest"></i></a></li>
+                            <li><a href="#"><i class="icofont-instagram"></i></a></li>
+                            <li><a href="#"><i class="icofont-linkedin"></i></a></li>
                         </ul>
                         <!-- End Social -->
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-12">
                     <div class="single-footer f-link">
-                        <h2>Quick Links</h2>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Home</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>About Us</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Services</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Our Cases</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Other Links</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Consuling</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Finance</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Testimonials</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>FAQ</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Contact Us</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <h2>Enlaces rápidos</h2>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Planes</a></li>
+                            <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Demo</a></li>
+                            <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Preguntas Frecuentes</a></li>
+                            <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Crear cuenta</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-12">
-                    <div class="single-footer">
-                        <h2>Open Hours</h2>
-                        <p>Lorem ipsum dolor sit ame consectetur adipisicing elit do eiusmod tempor incididunt.</p>
-                        <ul class="time-sidual">
-                            <li class="day">Monday - Fridayp <span>8.00-20.00</span></li>
-                            <li class="day">Saturday <span>9.00-18.30</span></li>
-                            <li class="day">Monday - Thusday <span>9.00-15.00</span></li>
+                    <div class="single-footer f-link">
+                        <h2>Horario de atención</h2>
+                        <ul>
+                            <li><a><i class="fa fa-caret-right" aria-hidden="true"></i> Domingo a Domingo</a></li>
+                            <li style="padding-left: 20px;"><a> 05:00 - 22:00</a></li>
+                            <li style="padding-left: 20px;"><a> + 057 321 244 98 69</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-12">
                     <div class="single-footer">
-                        <h2>Newsletter</h2>
-                        <p>subscribe to our newsletter to get allour news in your inbox.. Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+                        <h2>Boletín</h2>
+                        <p>Suscríbase a nuestro boletín para recibir todas nuestras noticias en su bandeja de entrada.</p>
                         <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-                            <input name="email" placeholder="Email Address" class="common-input" onfocus="this.placeholder = ''"
-                                   onblur="this.placeholder = 'Your email address'" required="" type="email">
+                            <input name="email" placeholder="Email" class="common-input" onfocus="this.placeholder = ''"
+                                   onblur="this.placeholder = 'Email'" required="" type="email">
                             <button class="button"><i class="icofont icofont-paper-plane"></i></button>
                         </form>
                     </div>
@@ -76,7 +61,7 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-12">
                     <div class="copyright-content">
-                        <p>© Copyright 2018  |  All Rights Reserved by <a href="https://www.wpthemesgrid.com" target="_blank">wpthemesgrid.com</a> </p>
+                        <p>© Copyright 2022 | Todos los derechos reservados a<a href="https://zaabra.co/" target="_blank">Zaabra.co</a> </p>
                     </div>
                 </div>
             </div>
@@ -85,7 +70,6 @@
     <!--/ End Copyright -->
 </footer>
 <!--/ End Footer Area -->
-
 
 <?php wp_footer(); ?>
 </body>
