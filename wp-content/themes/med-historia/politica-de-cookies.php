6fea047d4b5d6ce25d49e6dd1152aa010a6fdd2d
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-13 14:17:53 -0500

    politicas de privacidad

---
+++ wp-content/themes/med-historia/politica-de-cookies.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Template Name: Crear cuenta
+ */
+get_header();
+?>
+    <!-- Breadcrumbs -->
+    <div class="breadcrumbs overlay">
+        <div class="container">
+            <div class="bread-inner">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Política de Cookies</h2>
+                        <ul class="bread-list">
+                            <li><a href="index.html">Inicio</a></li>
+                            <li><i class="icofont-simple-right"></i></li>
+                            <li class="active">Política de cookies</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <section class="pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-justify">Esta página describe qué información recopilan, cómo la usamos y por qué a veces necesitamos almacenar estas cookies. También compartiremos cómo puede evitar que estas cookies se almacenen, sin embargo, esto puede degradar o "romper" ciertos elementos de la funcionalidad del sitio web.</p>
+                    <br>
+                    <h3>¿Qué son las cookies?</h3>
+                    <br>
+                    <p class="text-justify">Las cookies son pequeños archivos que se descargan en su computadora u otro dispositivo que utiliza para navegar por nuestro sitio web. Casi todos los sitios web profesionales utilizan cookies. Por lo general, una cookie incluye la siguiente información: el nombre del sitio web del que proviene; cuánto tiempo permanecerá la cookie en su computadora o dispositivo; y un valor (normalmente un número único generado aleatoriamente). Algunas cookies pueden incluir datos adicionales, en particular relacionados con su zona horaria o el idioma utilizado para navegar por sitios web.</p>
+                    <br>
+                    <h3>¿Qué tipo de cookies utilizamos?</h3>
+                    <br>
+                    <p class="text-justify">Algunas cookies están siempre activas cuando nos visita y no puede desactivarlas a menos que cambie la configuración de su navegador. A estas <b>cookies</b> las llamamos <b>necesarias</b> y las usamos para asegurarnos de que nuestros servicios digitales funcionen correctamente y permitan las funciones básicas de nuestros sitios web, como el inicio de sesión de usuario y la gestión de cuentas.</p>
+                    <p class="text-justify">También utilizamos <b>cookies funcionales</b> para que su experiencia sea más fácil de usar, en particular para recordar y almacenar su información en nuestros sitios web y para personalizar el contenido de nuestros servicios. También utilizamos <b>cookies de rendimiento</b> para observar cómo se utilizan nuestros servicios y obtener estadísticas de uso.</p>
+                    <p class="text-justify">Asimismo, utilizamos <b>cookies de terceros</b>. Estas cookies pueden rastrear cómo usa diferentes sitios web, incluido el nuestro. Por ejemplo, puede obtener las cookies de una empresa de redes sociales cuando inicie sesión en nuestro sitio web utilizando un complemento de redes sociales, pero puede desactivar estas cookies en cualquier momento.</p>
+                    <p class="text-justify">Las <b>cookies de marketing</b> utilizadas para publicidad dirigida también puede desactivarlas cuando así lo requiera.</p>
+                    <br>
+                    <h3>¿Cómo usamos las cookies?</h3>
+                    <br>
+                    <p class="text-justify">Para hacer un mejor uso de nuestro sitio web, le recomendamos que configure su dispositivo para que acepte todas las cookies. Sin embargo, utilizando la configuración de su dispositivo, puede deshabilitar o limitar ciertos tipos de cookies. Usamos cookies por una variedad de razones que se detallan a continuación:</p>
+                    <br>
+                    <ul class="lista pl-5">
+                        <li class="text-justify"><b>Para brindarle una mejor experiencia de usuario,</b> las cookies se utilizan para una serie de cosas que ayudan a mejorar su experiencia en línea. Pueden ayudar de diferentes maneras, que incluyen: garantizar que nuestros sitios web se carguen más rápido; mantenerlo conectado incluso en diferentes dispositivos; hacer que los inicios de sesión sean más rápidos y accesibles al recordar los detalles de su usuario; o recordar la forma en que utilizó nuestro servicio anteriormente de forma más rápida y accesible, asegurándose de que nuestras páginas estén optimizadas para su navegador o dispositivo.</li>
+                        <li class="text-justify"><b>Para que nuestros servicios se ajusten a sus preferencias</b>, las cookies pueden ayudarnos a presentarle contenido que satisfaga sus intereses y preferencias. Las cookies también nos ayudan a personalizar nuestro sitio web para usted.</li>
+                        <li><b>Para mejorar la seguridad</b>, utilizamos algunas cookies que nos permiten hacer seguros nuestros sitios web.</li>
+                        <li><b>Para la mejora de nuestros sitios web</b>, utilizamos cookies con el fin de mejorar nuestros servicios y predecir la forma en que los usuarios utilizan nuestros sitios web haciéndolos más útiles para nuestros usuarios.</li>
+                        <li><b>Para marketing</b>, algunas cookies las utilizamos para ofrecerle anuncios de nuestros servicios en cooperación con nuestros socios de marketing (Google y Facebook).</li>
+                    </ul>
+                    <br>
+                    <h3>Cookies de terceros</h3>
+                    <br>
+                    <p class="text-justify">Utilizamos cookies de terceros para perseguir nuestros intereses legítimos y mejorar nuestros sitios web para nuestros usuarios.</p>
+                    <br>
+                    <ul class="lista pl-5">
+                        <li class="text-justify">Usamos Google Analytics. Google Analytics es ampliamente utilizado por muchos sitios web profesionales en todo el mundo. Esta herramienta nos ayuda a comprender, predecir y ajustar nuestros sitios web para nuestros usuarios y nos ayuda a administrar nuestras bases de datos y a mejorar nuestro SEO.</li>
+                        <li class="text-justify">También utilizamos cookies de terceros para medir el uso de nuestro sitio web. En particular, estas cookies pueden registrar cuánto tiempo pasa en nuestro sitio web, cómo navega, etc.</li>
+                        <li class="text-justify">De igual manera, utilizamos cookies de terceros para probar nuestras nuevas funciones y productos. Estas cookies nos ayudan a garantizar que reciba una experiencia constante de nuestros servicios.</li>
+                        <li class="text-justify">También usamos botones y/o complementos de redes sociales en este sitio que le permiten conectarse con su red social de varias maneras. Para que estos funcionen, los siguientes sitios de redes sociales incluyen; Google y Facebook, que establecerán cookies a través de nuestro sitio utilizadas para mejorar su perfil en su sitio o contribuir a los datos que tienen para varios propósitos descritos en sus respectivas políticas de privacidad.</li>
+                        <li class="text-justify">Utilizamos soluciones técnicas proporcionadas por Facebook y Google para personalizar mejor nuestros servicios y ofrecerle una mejor experiencia de usuario.</li>
+                    </ul>
+                    <br>
+                    <h3>¿Cuánto tiempo almacenamos las cookies?</h3>
+                    <br>
+                    <p class="text-justify">Dependiendo del tipo de cookie, algunas se almacenan durante un corto período de tiempo mientras que otras se almacenan durante períodos más prolongados. A continuación, encontrará información más detallada.</p>
+                    <p class="text-justify"><b>Cookies de sesión:</b> estas cookies solo duran lo que la sesión que tiene con nosotros y se eliminan automáticamente cuando cierra su navegador.</p>
+                    <p class="text-justify"><b>Cookies permanentes:</b> estas cookies duran aun cuando su navegador se haya cerrado o su dispositivo se haya apagado, y están activas durante un período de tiempo definido en la propia cookie. Usamos cookies persistentes cuando necesitamos saber quién es usted durante más de una sesión de navegación. Por ejemplo, los usamos para recordar sus preferencias para la próxima vez que nos visite.</p>
+                    <br>
+                    <h3>Deshabilitar cookies</h3>
+                    <br>
+                    <p class="text-justify">Puede evitar la configuración de cookies ajustando la configuración de su navegador.</p>
+                    <p class="text-justify">Tenga en cuenta que deshabilitar las cookies afectará la funcionalidad de este y muchos otros sitios web que visite. La desactivación de las cookies generalmente resultará en la desactivación también de ciertas funciones y características de nuestros servicios y por lo tanto, se recomienda que no las desactive.</p>
+                    <p class="text-justify">Puede encontrar instrucciones sobre cómo hacerlo en la siguiente tabla:</p>
+                    <br>
+                    <ul class="lista pl-5">
+                        <li class="text-justify"><b>Chrome:</b> En el Navegador – 1. Configuración, 2. Configuración avanzada, 3. Privacidad y Seguridad, 4. Configuración del sitio web, 5. Cookies y datos del sitio. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Safari:</b> En el Navegador – 1. Preferencias, 2. Privacidad. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Internet Explorer:</b> En el Navegador – 1. Herramientas, 2. Opciones de Internet, 3. Privacidad, 4. Configuración avanzada. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Microsoft Edge:</b> En el Navegador – 1. Configuración, 2. Privacidad y seguridad. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Firefox:</b> En el Navegador – 1. Opciones, 2. Privacidad & Seguridad, 3. Cookies y datos del sitio. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Navegador para dispositivos Android:</b> En el Navegador – 1. Menú, 2. Configuración, 3. Privacidad. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                        <li class="text-justify"><b>Navegador para dispositivos IOS:</b> En el dispositivo – 1. Configuración, 2. Safari, 3. Privacidad y seguridad. Para mayor información, consulte el soporte o ayuda del Navegador.</li>
+                    </ul>
+                    <br>
+                    <p class="text-justify"><b>ZAABRA COLOMBIA SAS</b>, propietaria de la plataforma MedHistoria tendrá la plena libertad y autoridad para modificar, adaptar o cambiar el contenido de esta política de cookies en cualquier momento. Usted acepta y reconoce las actualizaciones de este documento, por medio de la fecha de elaboración que está visible en la plataforma.</p>
+                    <br>
+                    <p class="text-justify">Si usted tiene inquietudes adicionales sobre el uso de cookies o cualquier aspecto de esta política puede escribirnos a <a style="color: #093456;"  href="mailto:servicioalcliente@zaabra.com.co"><b>servicioalcliente@zaabra.com.co</b></a>, o en nuestro domicilio ubicado en el barrio José Joaquín Vargas Carrera 64 número 67b 89 - Interior 2 </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ End Register -->
+
+<?php get_footer();
